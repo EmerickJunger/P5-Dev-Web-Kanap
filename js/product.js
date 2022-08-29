7b879cf9e809f5560fbe9d47c176d47da6e0b67b
Emerick Junger <emerick.junger@gmail.com>
2022-08-29 13:56:04 +0200

    panier color and quantity

--- js/product.js
+++ js/product.js
@@ -10,7 +10,7 @@ async function fetchFromApi(id = "") {
     
 
 function getProduct() {
-    console.log(window.location)
+    console.log(window.location);
     try {
         fetchFromApi(window.location.search.split("?id=")[1]).then(
         
@@ -22,7 +22,7 @@ function getProduct() {
 
                 img.src = result.imageUrl;
                 img.alt = result.altTxt;
-                imgDiv.appendChild(img)
+                imgDiv.appendChild(img);
 
                 const title = document.querySelector("title");
                 title.innerText = result.name;
@@ -35,24 +35,79 @@ function getProduct() {
 
                 
                 for (let i=0; i<result.colors.length;i++){
-                    console.log(result.colors[i])
+                    console.log(result.colors[i]);
                     const colors = document.getElementById("colors");
                     let option = document.createElement("option");
                     option.value = result.colors[i];
                     option.innerText = result.colors[i];
                     colors.appendChild(option);
                 }
+
+
             }
         )
         const button = document.getElementById("addToCart");
+        /*le nombre de kanap doit etre compris entre 1 et 100
+                il faut donc creer une boucle" avec une valeur min de 1
+                et une max de 100*/
+        /* couleur choisi pour ajouter au panier sinon erreur */
         button.addEventListener('click', function(){
-            localStorage.setItem("panier","blue");   
-        })
+            /*let objJson = {
+                title : "result.name",
+                color : "result.colors",
+                quantity : "",
+            }
+            let objKanap = JSON.stringify(objJson);*/
+            //localStorage.setItem("panier","obj",objKanap);
+            let isValidQuantity = true;
+            const quantity = document.getElementById('quantity');
+            isValidQuantity = verifyQuantity(quantity.value);
 
-    
+            let isValidColor = true;
+            const select = document.getElementById('colors');
+            const color = select.options[select.selectedIndex].text;
+            isValidColor = verifyColor(color);
+            
+            if (isValidQuantity && isValidColor){
+                //Gestion localStorage
+            }
 
 
+
+        }
+        )
+        // pas de prix stocké dans le localstorage
     } catch (error) {
         console.error(error);
     } 
+}
+function verifyQuantity(quantity){
+    let span = document.createElement("span");
+    if (Number(quantity)<1){
+        
+        span.style.color = "red";
+        span.innerText = "La quantité doit être supérieur à 0";
+        const divItemContentSettingsQuantity = document.querySelector(".item__content__settings__quantity");
+        const br = document.createElement("br");
+        divItemContentSettingsQuantity.appendChild(br);
+        divItemContentSettingsQuantity.appendChild(span);
+        return false;
+    
+    } else 
+    {
+        span.remove();
+        return true;
+    };
+}
+
+function verifyColor(color){
+    let span = document.createElement("span");
+    console.log(color);
+    if (color.includes("--SVP, choisissez une couleur --")){
+        return false;
+
+    } else
+    {
+        return true;
+    }
 }
